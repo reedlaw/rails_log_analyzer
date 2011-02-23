3acdaf02027f6b71a1e971d3f057be77a90b0235
Reed G. Law <reedlaw@gmail.com>
2011-02-23 15:28:34 -0500

    work without query_stats

--- rails_log_analyzer.rb
+++ rails_log_analyzer.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
-require 'optparse'                                                                                                                                                                                                                                                                                                                                                                                               
+require 'rubygems'
+require 'ruby-debug'
+require 'optparse' 
 
 options = {:limit => 20}
 
@@ -102,32 +104,11 @@ begin
       controller = $1
       action = $2
       method = $3
-    elsif line =~ /Completed in ([0-9]+)ms \((View: ([0-9]+), DB: ([0-9]+)|DB: ([0-9]+)) ([0-9]+) queries\) \| ([0-9]+)/i
+    elsif line =~ /Completed in ([0-9]+)ms \((View: ([0-9]+), DB: ([0-9]+)\)) \| ([0-9]+)/i
       total_time = $1.to_i
-
-      unless $3.nil?
-        view_time = $3
-        db_time = $4.to_i
-        queries = $6.to_i
-        status = $7.to_i
-      else
-        db_time = $5.to_i
-        queries = $6.to_i
-        status = $7.to_i
-      end
-    elsif line =~ /Completed in ([0-9]+)ms \((View: ([0-9]+), DB: ([0-9]+)|DB: ([0-9]+)) ([0-9]+) queries\) \| ([0-9]+)/i
-      total_time = $1.to_i
-      
-      unless $3.nil?
-        view_time = $3
-        db_time = $4.to_i
-        queries = $6.to_i
-        status = $7.to_i
-      else
-        db_time = $5.to_i
-        queries = $6.to_i
-        status = $7.to_i
-      end
+      view_time = $3
+      db_time = $4.to_i
+      status = $5.to_i
     elsif line =~ /500 Error/i
       status = 500
     end
@@ -193,9 +174,6 @@ begin
   
   puts "Top slowest actions:"
   table_output(['Controller', 'Action', 'Total time [seconds]'], top_slowest_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x["total_time"]/1000]}.sort{|a,b| b[2] <=> a[2]})
-
-  puts "\nTop most DB queries actions:"
-  table_output(['Controller', 'Action', 'Queries count'], top_db_queries_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x["queries"]]}.sort{|a,b| b[2] <=> a[2]})
 
   puts "\nTop most requested actions:"
   table_output(['Controller', 'Action', 'Requested count'], top_most_requested_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x]}.sort{|a,b| b[2] <=> a[2]}[0 .. options[:limit] - 1])
